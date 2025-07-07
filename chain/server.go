f686cff6a145c3bccaee54a4742ffe78281bc3b3
jmsadair <jmsadair.swe@gmail.com>
2025-07-07 13:10:56 -0500

    Update server signatures

--- chain/server.go
+++ chain/server.go
@@ -26,14 +26,14 @@ func NewServer(address net.Addr, dialOpts ...grpc.DialOption) (*Server, error) {
 	return &Server{dialOpts: dialOpts, chainNode: chainNode}, nil
 }
 
-func (s *Server) Put(ctx context.Context, key string, value []byte) (*pb.PutResponse, error) {
-	return &pb.PutResponse{}, s.chainNode.Put(key, value)
+func (s *Server) Write(ctx context.Context, request *pb.WriteRequest) (*pb.WriteResponse, error) {
+	return nil, nil
 }
 
-func (s *Server) Get(ctx context.Context, key string) (*pb.GetResponse, error) {
-	value, err := s.chainNode.Get(key)
-	if err != nil {
-		return nil, err
-	}
-	return &pb.GetResponse{Value: value}, nil
+func (s *Server) Read(ctx context.Context, request *pb.ReadRequest) (*pb.ReadResponse, error) {
+	return nil, nil
+}
+
+func (s *Server) Backfill(request *pb.BackfillRequest, stream pb.ChainService_BackfillServer) (*pb.KeyValuePair, error) {
+	return nil, nil
 }
