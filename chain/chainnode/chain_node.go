32fb07bf4e610cbf87fd358b5d577d9ed529f6b3
jmsadair <jmsadair.swe@gmail.com>
2025-07-07 13:39:24 -0500

    Add initial logic for reading and writing keys

--- chain/chainnode/chain_node.go
+++ chain/chainnode/chain_node.go
@@ -1,30 +1,32 @@
 package chainnode
 
 import (
+	"errors"
 	"net"
 	"sync"
+	"sync/atomic"
+
+	"github.com/jmsadair/zebraos/chain/metadata"
 )
 
 type Storage interface {
-	Put(key string, value []byte) error
-	Get(key string) ([]byte, error)
-	Delete(key string) error
+	UncommittedWrite(key string, value []byte, version uint64) error
+	UncommittedWriteNewVersion(key string, value []byte) (uint64, error)
+	CommittedWrite(key string, value []byte, version uint64) error
+	CommittedRead(key string) ([]byte, error)
 }
 
 type Client interface {
-	Put(address net.Addr, key string, value []byte) error
-	Delete(address net.Addr, key string) error
+	Write(address net.Addr, key string, value []byte, version uint64) error
+	Read(address net.Addr, key string) ([]byte, error)
 }
 
 type ChainNode struct {
 	// The address of this node.
 	address net.Addr
-	// The address of this node's predecessor in the chain.
-	// This value is nil if this node is the head.
-	pred net.Addr
-	// The address of this node's successor in the chain.
-	// This value is nil if this node is the tail.
-	succ net.Addr
+	// The metadata that describes the chain this node is a member of.
+	// This value will be nil unless the node has been added to a chain.
+	chainMetadata atomic.Pointer[metadata.ChainMetadata]
 	// The local storage for this node.
 	storage Storage
 	// A client for communicating with other nodes in the chain.
@@ -33,54 +35,44 @@ type ChainNode struct {
 	mu sync.RWMutex
 }
 
-func NewChainNode(address net.Addr, predecessor net.Addr, successor net.Addr, storage Storage, client Client) *ChainNode {
-	return &ChainNode{address: address, pred: predecessor, succ: successor, storage: storage, client: client}
+func NewChainNode(address net.Addr, storage Storage, client Client) *ChainNode {
+	return &ChainNode{address: address, storage: storage, client: client}
 }
 
-func (c *ChainNode) Predecessor() net.Addr {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	return c.pred
-}
-
-func (c *ChainNode) Successor() net.Addr {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	return c.succ
-}
-
-func (c *ChainNode) SetPredecessor(predecessor net.Addr) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.pred = predecessor
-}
-
-func (c *ChainNode) SetSuccessor(successor net.Addr) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.succ = successor
-}
-
-func (c *ChainNode) Put(key string, value []byte) error {
-	if err := c.storage.Put(key, value); err != nil {
+func (c *ChainNode) WriteWithVersion(key string, value []byte, version uint64) error {
+	metadata := c.chainMetadata.Load()
+	if metadata == nil {
+		return errors.New("not a member of a chain")
+	}
+	if metadata.IsTail(c.address) {
+		return c.storage.CommittedWrite(key, value, version)
+	}
+	if err := c.storage.UncommittedWrite(key, value, version); err != nil {
 		return err
 	}
-	if c.Successor() == nil {
-		return nil
-	}
-	return c.client.Put(c.Successor(), key, value)
-}
 
-func (c *ChainNode) Get(key string) ([]byte, error) {
-	return c.storage.Get(key)
-}
-
-func (c *ChainNode) Delete(key string) error {
-	if err := c.storage.Delete(key); err != nil {
+	successor, err := metadata.Successor(c.address)
+	if err != nil {
 		return err
 	}
-	if c.Successor() == nil {
-		return nil
+	return c.client.Write(successor, key, value, version)
+}
+
+func (c *ChainNode) ReplicatedWrite(key string, value []byte) error {
+	metadata := c.chainMetadata.Load()
+	if metadata == nil {
+		return errors.New("not a member of a chain")
 	}
-	return c.client.Delete(c.Successor(), key)
+	if !metadata.IsHead(c.address) {
+		return errors.New("only the head of a chain can initiate replicated writes")
+	}
+	version, err := c.storage.UncommittedWriteNewVersion(key, value)
+	if err != nil {
+		return err
+	}
+	return c.client.Write(c.address, key, value, version)
+}
+
+func (c *ChainNode) Read(key string) ([]byte, error) {
+	return nil, nil
 }
