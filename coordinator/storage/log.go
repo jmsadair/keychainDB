ca7243d7a119569a812e61ae0745494ed9fefd2f
James Adair <jmsadair.swe@gmail.com>
2025-08-26 17:26:06 -0500

    Implement raft log (#23)

---
+++ coordinator/storage/log.go
@@ -0,0 +1,159 @@
+package storage
+
+import (
+	"encoding/binary"
+
+	"github.com/dgraph-io/badger/v4"
+	"github.com/hashicorp/raft"
+	pb "github.com/jmsadair/keychain/proto/pbcoordinator"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func logToBytes(log *raft.Log) ([]byte, error) {
+	protoLog := &pb.Log{
+		Index:      log.Index,
+		Term:       log.Term,
+		Type:       uint32(log.Type),
+		Data:       log.Data,
+		Extensions: log.Extensions,
+		AppendedAt: timestamppb.New(log.AppendedAt.UTC()),
+	}
+	return proto.Marshal(protoLog)
+}
+
+func bytesToLog(b []byte, log *raft.Log) error {
+	protoLog := &pb.Log{}
+	if err := proto.Unmarshal(b, protoLog); err != nil {
+		return err
+	}
+
+	log.Index = protoLog.Index
+	log.Term = protoLog.Term
+	log.Type = raft.LogType(protoLog.Type)
+	log.Data = protoLog.Data
+	log.Extensions = protoLog.Extensions
+	log.AppendedAt = protoLog.AppendedAt.AsTime()
+
+	return nil
+}
+
+// PersistentLog is a persistent storage system for raft log entries.
+type PersistentLog struct {
+	db *badger.DB
+}
+
+// NewPersistentLog creates a new log at the provided path.
+func NewPersistentLog(dbpath string) (*PersistentLog, error) {
+	db, err := badger.Open(badger.DefaultOptions(dbpath))
+	if err != nil {
+		return nil, err
+	}
+	return &PersistentLog{db: db}, nil
+}
+
+// FirstIndex returns the index of the first log entry. If there are no log entries, then zero is returned.
+func (pl *PersistentLog) FirstIndex() (uint64, error) {
+	var first uint64
+	firstPtr := &first
+
+	err := pl.db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchValues = false
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		it.Rewind()
+		if it.Valid() {
+			*firstPtr = binary.BigEndian.Uint64(it.Item().Key())
+		}
+		return nil
+	})
+
+	return *firstPtr, err
+}
+
+// LastIndex returns the index of the last log entry. If there are no log entries, then zero is returned.
+func (pl *PersistentLog) LastIndex() (uint64, error) {
+	var last uint64
+	lastPtr := &last
+
+	err := pl.db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchValues = false
+		opts.Reverse = true
+		it := txn.NewIterator(opts)
+		defer it.Close()
+
+		it.Rewind()
+		if it.Valid() {
+			*lastPtr = binary.BigEndian.Uint64(it.Item().Key())
+		}
+
+		return nil
+	})
+
+	return *lastPtr, err
+}
+
+// GetLog gets the log entry at the provided index.
+func (pl *PersistentLog) GetLog(index uint64, log *raft.Log) error {
+	return pl.db.View(func(txn *badger.Txn) error {
+		key := make([]byte, 8)
+		binary.BigEndian.PutUint64(key, index)
+		item, err := txn.Get(key)
+		if err != nil {
+			return err
+		}
+		value, err := item.ValueCopy(nil)
+		if err != nil {
+			return err
+		}
+		return bytesToLog(value, log)
+	})
+}
+
+// StoreLog stores the provided log entry in the log.
+func (pl *PersistentLog) StoreLog(log *raft.Log) error {
+	return pl.StoreLogs([]*raft.Log{log})
+}
+
+// StoreLogs stores multiple log entries.
+func (pl *PersistentLog) StoreLogs(logs []*raft.Log) error {
+	return pl.db.Update(func(txn *badger.Txn) error {
+		for _, log := range logs {
+			key := make([]byte, 8)
+			binary.BigEndian.PutUint64(key, log.Index)
+			value, err := logToBytes(log)
+			if err != nil {
+				return err
+			}
+			txn.Set(key, value)
+		}
+		return nil
+	})
+}
+
+// DeleteRange deletes all log entries with an index in the provided range inclusive.
+func (pl *PersistentLog) DeleteRange(min uint64, max uint64) error {
+	return pl.db.Update(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchValues = false
+
+		it := txn.NewIterator(opts)
+		defer it.Close()
+
+		minKey := make([]byte, 8)
+		binary.BigEndian.PutUint64(minKey, min)
+		for it.Seek(minKey); it.Valid(); it.Next() {
+			key := it.Item().KeyCopy(nil)
+			if binary.BigEndian.Uint64(key) > max {
+				break
+			}
+			if err := txn.Delete(key); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+}
