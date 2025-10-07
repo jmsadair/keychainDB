b468b687fd721547885dbbd8ab9c1929398c2bb4
James Adair <jmsadair.swe@gmail.com>
2025-10-07 16:24:08 -0500

    Fix utilities

--- coordinator/service.go
+++ coordinator/service.go
@@ -3,7 +3,7 @@ package coordinator
 import (
 	"context"
 
-	chaingrpc "github.com/jmsadair/keychain/chain/grpc"
+	chainclient "github.com/jmsadair/keychain/chain/client"
 	"github.com/jmsadair/keychain/coordinator/node"
 	"github.com/jmsadair/keychain/coordinator/raft"
 	"github.com/jmsadair/keychain/coordinator/server"
@@ -16,7 +16,7 @@ type Service struct {
 	// HTTP server implementation.
 	HTTPServer *server.HTTPServer
 	// gRPC server implementation.
-	RPCServer *server.RPCServer
+	GRPCServer *server.RPCServer
 	// The raft consensus protocol implementation.
 	Raft *raft.RaftBackend
 	// The coordinator implementation.
@@ -38,14 +38,14 @@ func NewService(
 	if err != nil {
 		return nil, err
 	}
-	tn, err := chaingrpc.NewClient(dialOpts...)
+	tn, err := chainclient.NewClient(dialOpts...)
 	if err != nil {
 		return nil, err
 	}
 	coordinator := node.NewCoordinator(httpAddr, tn, rb)
-	httpSrv := &server.HTTPServer{Address: httpAddr, GRPCAddress: grpcAddr, DialOptions: dialOpts}
-	grpcSrv := server.NewServer(grpcAddr, coordinator)
-	return &Service{HTTPServer: httpSrv, RPCServer: grpcSrv, Coordinator: coordinator, Raft: rb}, nil
+	httpServer := &server.HTTPServer{Address: httpAddr, GRPCAddress: grpcAddr, DialOptions: dialOpts}
+	gRPCServer := server.NewServer(grpcAddr, coordinator)
+	return &Service{HTTPServer: httpServer, GRPCServer: gRPCServer, Coordinator: coordinator, Raft: rb}, nil
 }
 
 // Run runs the service.
@@ -58,7 +58,7 @@ func (s *Service) Run(ctx context.Context) error {
 		return s.HTTPServer.Run(ctx)
 	})
 	g.Go(func() error {
-		return s.RPCServer.Run(ctx)
+		return s.GRPCServer.Run(ctx)
 	})
 	return g.Wait()
 }
